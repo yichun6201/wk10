76d1f5871d2570ded720dfac5c05b8db0d726248
yichunchun <39977904+yichunchun@users.noreply.github.com>
2018-11-21 11:16:22 +0800

    Add files via upload

---
+++ wk10/wk10.ino
@@ -0,0 +1,83 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char* ssid = "miffyqq";
+const char* password = "qqmiffymiffy";
+
+WebServer server(80);
+String website;
+
+void handleRoot() {
+  server.send(200, "text/html",website);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+
+  pinMode(32,INPUT);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+  buildWeb();
+  server.on("/", handleRoot);
+  server.on("/sensors", handleSensorData);
+  server.on("/inline", []() {
+    server.send(200, "text/html",website);
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop(void) {
+  server.handleClient();
+  
+}
+
+void handleSensorData(){
+  int sensor;
+  sensor=analogRead(32);
+  sensor=map(sensor,0,4095,0,255);
+  String json="{\"vr\":";
+  json+=sensor;
+  json+="}";
+  server.send(200, "application/json",json);
+}
+
+void buildWeb(){
+website+="<!DOCTYPE html><html lang=\"en\"><head> <meta charset=\"UTF-8\"> <script src=\"https://code.jquery.com/jquery-2.2.4.min.js\"></script> <script>setInterval(requestData, 500); function requestData(){$.get(\"/sensors\") .done(function(data){if(data){$(\"#resis\").text(data.vr);}else{$(\"#resis\").text(\"?\");}}) .fail(function(){console.log(\"fail\");});}</script> <title>VR Reader</title></head><body> <div class=\"container\"> <p>Variable Resistor=<span id=\"resis\"></span></p></div></body></html>";
+}
